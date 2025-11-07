ff8eee192cac4ece7118fa5ad72610dd714fb449
Roberto Piva <roberto.piva@zucchetti.it>
2025-11-07 11:39:36 +0100

    Aggiornamento di nomi e bozza dei trigger

--- okd-cluster/tofu/env-to-tfvars.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "# Auto-generated from .env" > secrets.auto.tfvars
-
-while IFS='=' read -r key value; do
-  [[ $key == \#* || -z $key ]] && continue
-  echo "$key = \"${value}\"" >> secrets.auto.tfvars
-done < ../.env
--- okd-cluster/tofu/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "vm_names" {
-  value = proxmox_vm_qemu.okd_nodes[*].name
-}
-
-output "vm_ips" {
-  value = var.node_ips
-}
